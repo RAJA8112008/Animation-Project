410f28aad3450935f71770cf73cf90c59a8cd5cb
Raja <kraj9380286@gmail.com>
2025-02-09 12:48:44 +0530

    commited

--- LAGRAV/index.js
+++ LAGRAV/index.js
@@ -1,13 +1,11 @@
-import LocomotiveScroll from 'locomotive-scroll';
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
-
 function circlemouseFollower() {
     window.addEventListener("mousemove", function(dets) {
         document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-    });
+    })
 }
-circlemouseFollower();
+circlemouseFollower()
