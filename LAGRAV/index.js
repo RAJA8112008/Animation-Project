cc2b42a52ca1d808ec33e6ecdd826122257a155c
Raja <kraj9380286@gmail.com>
2025-02-09 14:05:32 +0530

    commited

--- LAGRAV/index.js
+++ LAGRAV/index.js
@@ -1,11 +1,37 @@
-
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
-function circlemouseFollower() {
-    window.addEventListener("mousemove", function(dets) {
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+
+function firstpageAnim() {
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: "expo.inOut",
     })
+    .to(".boundingelem", {
+        y: 0,
+        ease: "expo.inOut",
+        duration: 2,
+        stagger: 0.2
+    })
+    .from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        ease: "expo.inOut"
+    }, "-=0.5"); 
 }
-circlemouseFollower()
+
+function circlemouseFollower() {
+    window.addEventListener("mousemove", function (dets) {
+        document.querySelector("#minicircle").style.transform = 
+        `translate(${dets.clientX}px, ${dets.clientY}px)`; // Corrected syntax with backticks
+    });
+}
+
+circlemouseFollower();
+firstpageAnim();
